8f21ac0bb2eba0c12c7d7f61a1194a8f67cb9a6d
ASChirag <139351675+ASChirag@users.noreply.github.com>
2024-02-10 18:47:41 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -7,17 +7,16 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-# Importing all images
+
 imgBackground = cv2.imread("Resources/Background.png")
 imgGameOver = cv2.imread("Resources/gameOver.png")
 imgBall = cv2.imread("Resources/Ball.png", cv2.IMREAD_UNCHANGED)
 imgBat1 = cv2.imread("Resources/bat1.png", cv2.IMREAD_UNCHANGED)
 imgBat2 = cv2.imread("Resources/bat2.png", cv2.IMREAD_UNCHANGED)
 
-# Hand Detector
+
 detector = HandDetector(detectionCon=0.8, maxHands=2)
 
-# Variables
 ballPos = [100, 100]
 speedX = 15
 speedY = 15
@@ -29,13 +28,12 @@ while True:
     img = cv2.flip(img, 1)
     imgRaw = img.copy()
 
-    # Find the hand and its landmarks
-    hands, img = detector.findHands(img, flipType=False)  # with draw
+ 
+    hands, img = detector.findHands(img, flipType=False)  
 
-    # Overlaying the background image
+
     img = cv2.addWeighted(img, 0.2, imgBackground, 0.8, 0)
 
-    # Check for hands
     if hands:
         for hand in hands:
             x, y, w, h = hand['bbox']
@@ -57,7 +55,7 @@ while True:
                     ballPos[0] -= 30
                     score[1] += 1
 
-    # Game Over
+
     if ballPos[0] < 40 or ballPos[0] > 1200:
         gameOver = True
 
@@ -66,17 +64,16 @@ while True:
         cv2.putText(img, str(score[1] + score[0]).zfill(2), (585, 360), cv2.FONT_HERSHEY_COMPLEX,
                     2.5, (200, 0, 200), 5)
 
-    # If game not over move the ball
+  
     else:
 
-        # Move the Ball
         if ballPos[1] >= 500 or ballPos[1] <= 10:
             speedY = -speedY
 
         ballPos[0] += speedX
         ballPos[1] += speedY
 
-        # Draw the ball
+    
         img = cvzone.overlayPNG(img, imgBall, ballPos)
 
         cv2.putText(img, str(score[0]), (300, 650), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 255, 255), 5)
